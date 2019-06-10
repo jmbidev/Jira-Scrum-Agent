79f4a02433322bbccf5935383cc2173988b28eea
Jeremias <jmb.idev@gmail.com>
2019-06-09 21:14:10 -0300

    Project Initialization

---
+++ src/main/java/application/controllers/MainController.java
@@ -0,0 +1,20 @@
+package application.controllers;
+
+import com.atlassian.connect.spring.AtlassianHostUser;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    /*
+    @RequestMapping(value = "/home/home", method = RequestMethod.GET)
+    @ResponseBody
+    public String helloWorld(@AuthenticationPrincipal AtlassianHostUser hostUser){
+        return "Hello world";
+    }
+
+     */
+}
