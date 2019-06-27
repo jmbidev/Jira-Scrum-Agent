8d4e887d83f1e4afe1b9333355413baaf6c0aeb6
jisegura <segurajuanignacio@outlook.com>
2019-06-26 21:36:36 -0300

    add angular front

--- src/main/java/application/controllers/MainController.java
+++ src/main/java/application/controllers/MainController.java
@@ -1,20 +1,21 @@
 package application.controllers;
 
 import com.atlassian.connect.spring.AtlassianHostUser;
+import com.atlassian.connect.spring.IgnoreJwt;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
-    /*
-    @RequestMapping(value = "/home/home", method = RequestMethod.GET)
-    @ResponseBody
-    public String helloWorld(@AuthenticationPrincipal AtlassianHostUser hostUser){
-        return "Hello world";
-    }
 
-     */
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    @ResponseBody
+    public ModelAndView helloWorld(@AuthenticationPrincipal AtlassianHostUser hostUser){
+        // TODO: Almacenar ID Usuario en una variable de sesion
+        ModelAndView model = new ModelAndView();
+        model.setViewName("JiraAngular/index.html");
+        return model;
+    }
 }
